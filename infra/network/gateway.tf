bce40eb132b20cdaca410755f82b09e58450e5c1
Masato Takeuchi <tamaco489@gmail.com>
2025-03-30 04:07:39 +0900

    feature network (#1)
    * add vpc and subnet
    
    * add internet gateway
    
    * add eip and nat-gateway
    
    * add route table

---
+++ infra/network/gateway.tf
@@ -0,0 +1,20 @@
+resource "aws_internet_gateway" "ssr_web_app" {
+  vpc_id = aws_vpc.ssr_web_app.id
+
+  tags = {
+    Env     = var.env
+    Project = var.project
+    Name    = "${local.fqn}-igw"
+  }
+}
+
+resource "aws_nat_gateway" "ssr_web_app" {
+  allocation_id = aws_eip.nat_gw.id
+  subnet_id     = aws_subnet.public_subnet["a"].id
+
+  tags = {
+    Env     = var.env
+    Project = var.project
+    Name    = "${var.env}-${var.project}-ngw"
+  }
+}
---
+++ infra/network/subnet.tf
@@ -0,0 +1,29 @@
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.ssr_web_app.id
+  for_each                = var.public_subnet
+  cidr_block              = each.value["cidr"]
+  availability_zone       = "${var.region}${each.value["az"]}"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Env     = var.env
+    Project = var.project
+    Name    = "${local.fqn}-public-subnet-${each.value["az"]}"
+    AZ      = "${each.value["az"]}"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.ssr_web_app.id
+  for_each                = var.private_subnet
+  cidr_block              = each.value["cidr"]
+  availability_zone       = "${var.region}${each.value["az"]}"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Env     = var.env
+    Project = var.project
+    Name    = "${local.fqn}-private-subnet-${each.value["az"]}"
+    AZ      = "${each.value["az"]}"
+  }
+}
