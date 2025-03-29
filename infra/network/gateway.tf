15c1bbc0f9a88026b5c4158fcf01162fc10a902f
tamaco489 <tamaco489@gmail.com>
2025-03-30 04:03:44 +0900

    add eip and nat-gateway

--- infra/network/gateway.tf
+++ infra/network/gateway.tf
@@ -8,13 +8,13 @@ resource "aws_internet_gateway" "ssr_web_app" {
   }
 }
 
-# resource "aws_nat_gateway" "ssr_web_app" {
-#   allocation_id = aws_eip.nat_gw.id
-#   subnet_id     = aws_subnet.public_subnet["a"].id
+resource "aws_nat_gateway" "ssr_web_app" {
+  allocation_id = aws_eip.nat_gw.id
+  subnet_id     = aws_subnet.public_subnet["a"].id
 
-#   tags = {
-#     Env     = var.env
-#     Project = var.project
-#     Name    = "${var.env}-${var.project}-ngw"
-#   }
-# }
+  tags = {
+    Env     = var.env
+    Project = var.project
+    Name    = "${var.env}-${var.project}-ngw"
+  }
+}
