c4bbd90fca716cc65f298f85426d8f21e91add40
tamaco489 <tamaco489@gmail.com>
2025-03-30 21:15:42 +0900

    add capacity providers

--- infra/ecs/ecs.tf
+++ infra/ecs/ecs.tf
@@ -12,3 +12,25 @@ resource "aws_ecs_cluster" "main" {
     Name    = "${local.fqn}-ecs-cluster"
   }
 }
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name = aws_ecs_cluster.main.name
+
+  # NOTE: 本番運用の際にはスポットインスタンスだけではなく、FARGATEも確保しておく必要あり。
+  capacity_providers = [
+    # "FARGATE",
+    "FARGATE_SPOT",
+  ]
+
+  # default_capacity_provider_strategy {
+  #   base              = 1
+  #   weight            = 1
+  #   capacity_provider = "FARGATE"
+  # }
+
+  default_capacity_provider_strategy {
+    base              = 0
+    weight            = 1
+    capacity_provider = "FARGATE_SPOT"
+  }
+}
