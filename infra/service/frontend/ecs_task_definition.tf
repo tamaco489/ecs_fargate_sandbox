33a8553f1a2485075fb2172fb9b1c867db67bca9
tamaco489 <tamaco489@gmail.com>
2025-03-31 00:10:06 +0900

    add ecs task definition for frontend service

---
+++ infra/service/frontend/ecs_task_definition.tf
@@ -0,0 +1,48 @@
+# DOC: [AWS] https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html
+# DOC: [terraform] https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_task_definition
+resource "aws_ecs_task_definition" "frontend_service" {
+  family                   = "${local.fqn}-frontend"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+  container_definitions = jsonencode([
+    {
+      name      = "frontend"
+      image     = "vercel/next.js:latest"
+      cpu       = 256
+      memory    = 512
+      essential = true // コンテナが停止した際にタスク全体も停止させる
+
+      portMappings = [
+        {
+          containerPort = 3000
+          protocol      = "tcp"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs_frontend_service.name
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+
+  tags = {
+    Env     = var.env
+    Project = var.project
+    Name    = "${local.fqn}-ecs-frontend-service"
+  }
+}
