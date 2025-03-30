681ba8e3543726c523a1c5a6562e6c0cc7be4fea
tamaco489 <tamaco489@gmail.com>
2025-03-30 14:50:19 +0900

    fix variables

--- infra/network/variables.tf
+++ infra/network/variables.tf
@@ -8,7 +8,7 @@ variable "env" {
 }
 
 variable "project" {
-  description = "The project name"
+  description = "The project in which the network will be created"
   type        = string
   default     = "ssr-web-application"
 }
