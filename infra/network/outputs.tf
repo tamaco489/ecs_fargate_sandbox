6ae70eafdd7bbad79a7739fbe9783e05a43ed3ac
tamaco489 <tamaco489@gmail.com>
2025-03-30 15:10:55 +0900

    fix network resource name

--- infra/network/outputs.tf
+++ infra/network/outputs.tf
@@ -1,8 +1,8 @@
 output "vpc" {
   value = {
-    arn                = aws_vpc.ssr_web_app.arn
-    id                 = aws_vpc.ssr_web_app.id
-    cidr_block         = aws_vpc.ssr_web_app.cidr_block
+    arn                = aws_vpc.main.arn
+    id                 = aws_vpc.main.id
+    cidr_block         = aws_vpc.main.cidr_block
     public_subnet_ids  = [for s in aws_subnet.public_subnet : s.id]
     private_subnet_ids = [for s in aws_subnet.private_subnet : s.id]
   }
