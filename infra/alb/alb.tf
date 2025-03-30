8014b139d908615936559c3e8b2e507057990bc5
tamaco489 <tamaco489@gmail.com>
2025-03-30 18:17:10 +0900

    add http listener

--- infra/alb/alb.tf
+++ infra/alb/alb.tf
@@ -10,11 +10,36 @@ resource "aws_alb" "main" {
   idle_timeout               = 60 * 5 // 5 seconds
   client_keep_alive          = 60 * 5 // デフォルトだと3600になるがアイドル時間を10分が優先され混乱するので明示的に指定（ssrのため長時間維持する必要ない想定）
   enable_http2               = true
-  preserve_host_header = true
+  preserve_host_header       = true
 
   tags = {
     Env     = var.env
     Project = var.project
     Name    = "${local.fqn}-alb"
+  }
+}
+
+resource "aws_alb_listener" "http_listener" {
+  load_balancer_arn = aws_alb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  // NOTE: デフォルトアクションは一旦検証のため200で設定、後で404に変える
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "application/json"
+      status_code  = "200"
+      message_body = jsonencode({
+        message = "ok"
+        status  = "success"
+      })
+    }
+  }
+
+  tags = {
+    Env     = var.env
+    Project = var.project
+    Name    = "${local.fqn}-alb-http-listener"
   }
 }
