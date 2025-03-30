edb0efb6a6cbdb82abec2ba3b7acb80d977b247d
tamaco489 <tamaco489@gmail.com>
2025-03-30 20:30:35 +0900

    fix alb

--- infra/alb/outputs.tf
+++ infra/alb/outputs.tf
@@ -1,14 +1,14 @@
 output "alb" {
   value = {
-    arn                 = aws_alb.main.arn
-    id                  = aws_alb.main.id
-    zone_id             = aws_alb.main.zone_id
-    name                = aws_alb.main.name
-    dns_name            = aws_alb.main.dns_name
+    arn                 = aws_alb.public.arn
+    id                  = aws_alb.public.id
+    zone_id             = aws_alb.public.zone_id
+    name                = aws_alb.public.name
+    dns_name            = aws_alb.public.dns_name
     http_listener_arn  = aws_alb_listener.http_listener.arn
     # https_listener_arn  = aws_alb_listener.https_listener.arn
-    security_group_arn  = aws_security_group.alb.arn
-    security_group_id   = aws_security_group.alb.id
-    security_group_name = aws_security_group.alb.name
+    security_group_arn  = aws_security_group.public_alb.arn
+    security_group_id   = aws_security_group.public_alb.id
+    security_group_name = aws_security_group.public_alb.name
   }
 }
